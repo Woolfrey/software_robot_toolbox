340e862e23559e403b12b630d5b6bbc36416c5cc
Woolfrey <62581255+Woolfrey@users.noreply.github.com>
2021-05-13 11:54:18 +1000

    Obsolete

--- @SerialLink/velocityControl.m
+++
@@ -1,8 +0,0 @@
-%% SerialLink.velocityControl()
-% Jon Woolfrey
-%
-% This function returns the joint torques required to drive the joints at a
-% given velocity.
-
-function ret = velocityControl(obj,vel)
-end
